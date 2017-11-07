0083463aeb289965bc320dd2d1cfeac7933c72f4
Iván Mercado <c_ivanmercado@hotmail.com>
2017-11-06 19:27:15 -0500

    add functions and firebase deploy configuration

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const exporterBuffer = require('sass-export').buffer;
+const functions = require('firebase-functions');
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.getSassJson = functions.https.onRequest((request, response) => {
+exporterBuffer([new Buffer(request.body)]).then((result) => {response.send(result)
+});
+console.log("testing");
+ console.log(request.body);
+});
